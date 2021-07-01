760344e09d2d8390ee5314d9ca23c29a8d895fbf
ness0001 <kevinmtorrez@gmail.com>
2021-07-01 12:31:46 -0300

    inicio

---
+++ part1/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+const App = () => {
+ // const msj = 'Hola React'
+  return (
+    <div className="App">
+      <h1>sdasd</h1>
+      
+    </div>
+  );
+}
+
+export default App;
