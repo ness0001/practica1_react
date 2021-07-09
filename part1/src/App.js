22c27ba055654120b0f3f04b4a2acc827b066747
ness0001 <kevinmtorrez@gmail.com>
2021-07-09 13:12:44 -0300

    first commit

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,12 +1,22 @@
 
 import './App.css';
+import Message from './Message.js';
+
+
+const Description = () =>{
+  return <p>Bootacamp Example</p>
+}
 
 const App = () => {
  // const msj = 'Hola React'
   return (
     <div className="App">
-      <h1>sdasd</h1>
-      
+
+     <Message color='green' message='Hola mundo'/> 
+     <Message color='blue' message='desde '/> 
+     <Message color='violet' message='props'/> 
+     <Description/> 
+            
     </div>
   );
 }
---
+++ web-ejemplo/scrip.js
@@ -0,0 +1,12 @@
+console.log('hola mundo');
+
+let firstName = 'Miguel';
+
+const lastName = 4;
+
+console.log(firstName.toUpperCase())
+
+const list = []
+list.push(2,3)
+list.pop()
+console.log(list)
